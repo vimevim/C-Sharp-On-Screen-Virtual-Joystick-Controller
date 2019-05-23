6d746c17e2c8a5d1c7a747b3c7b684641ce616d0
Hasan SEZER <vimevim@users.noreply.github.com>
2019-05-23 10:00:22 +0300

    eksikler var, bir çok şey eklendi panel üzerinde tıklama yapıldığında joystick orada başlıyor

---
+++ cSharpOnScreenVirtualJoystickController/joystick.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace cSharpOnScreenVirtualJoystickController
+{
+    partial class joystick
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(joystick));
+            this.joystickArea = new System.Windows.Forms.Panel();
+            this.movingPart = new System.Windows.Forms.PictureBox();
+            this.joystickArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.movingPart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // joystickArea
+            // 
+            this.joystickArea.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("joystickArea.BackgroundImage")));
+            this.joystickArea.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.joystickArea.Controls.Add(this.movingPart);
+            this.joystickArea.Location = new System.Drawing.Point(0, 0);
+            this.joystickArea.Name = "joystickArea";
+            this.joystickArea.Size = new System.Drawing.Size(350, 350);
+            this.joystickArea.TabIndex = 3;
+            this.joystickArea.Click += new System.EventHandler(this.joystickArea_Click);
+            this.joystickArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.joystickArea_MouseDown);
+            this.joystickArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.joystickArea_MouseMove);
+            this.joystickArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.joystickArea_MouseUp);
+            // 
+            // movingPart
+            // 
+            this.movingPart.BackColor = System.Drawing.Color.Transparent;
+            this.movingPart.Enabled = false;
+            this.movingPart.Image = ((System.Drawing.Image)(resources.GetObject("movingPart.Image")));
+            this.movingPart.Location = new System.Drawing.Point(150, 150);
+            this.movingPart.Name = "movingPart";
+            this.movingPart.Size = new System.Drawing.Size(50, 50);
+            this.movingPart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.movingPart.TabIndex = 0;
+            this.movingPart.TabStop = false;
+            // 
+            // joystick
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.joystickArea);
+            this.Name = "joystick";
+            this.Text = "joystick";
+            this.joystickArea.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.movingPart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel joystickArea;
+        private System.Windows.Forms.PictureBox movingPart;
+    }
+}
+
---
+++ cSharpOnScreenVirtualJoystickController/joystick.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpOnScreenVirtualJoystickController
+{
+    public partial class joystick : Form
+    {
+        public joystick()
+        {
+            InitializeComponent();
+
+        }
+
+        private void joystickArea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Point mouseDownLocation;
+        int cursorOriginX, cursorOriginY;
+        int movingPartX, movingPartY;
+        double hypotenuse, angle;
+        private void joystickArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownLocation = e.Location;
+                movingPart.Left = movingPartX;
+                movingPart.Top = movingPartY;
+            }
+        }
+        Point merkezeAl = new Point(25, 25);
+        private void joystickArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            hypotenuse = Math.Sqrt(cursorOriginX * cursorOriginX + cursorOriginY * cursorOriginY);
+
+            angle = Math.Acos(cursorOriginY/hypotenuse);
+            if(cursorOriginX<0)
+            {
+                angle = angle * 180 / Math.PI;
+            }
+            else
+            {
+                angle -= angle * 180 / Math.PI;
+                angle += 360;
+            }
+
+            movingPartX = e.X - (movingPart.Width / 2);
+            movingPartY = e.Y - (movingPart.Height / 2);
+            cursorOriginX = e.X - (joystickArea.Width / 2);
+            cursorOriginY = e.Y - (joystickArea.Height / 2);
+
+
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                if (hypotenuse<150)
+                {
+                    if (cursorOriginX < 125 && cursorOriginX > -125)//x ekseninde 100 ile -100 arasından çıkartmıyor
+                    {
+                        movingPart.Left = movingPartX;
+                    }
+                    if (cursorOriginY < 125 && cursorOriginY > -125)//y ekseninde 100 ile -100 arasından çıkartmıyor
+                    {
+                        movingPart.Top = movingPartY;
+                    }
+                }
+
+
+            }
+            else
+            {
+                joystickArea_MouseUp(null, null);
+            }
+            this.Text = cursorOriginX + " " + cursorOriginY+"   "+movingPartX+" "+movingPartY;
+        }
+
+        private void joystickArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            movingPart.Left = 150;
+            movingPart.Top = 150;
+        }
+    }
+}
